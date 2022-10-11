2062b196455577095a4bab2e5e75da91584e5888
takuma-ru(rasp) <kcattdev@gmail.com>
2022-10-11 02:19:48 +0100

    [Add] set up

---
+++ main.py
@@ -0,0 +1,15 @@
+import argparse
+import defs.add_sensing
+
+def add_sensing():
+  defs.add_sensing.test()
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('function_name', type=str, help='set fuction name in this file')
+  parser.add_argument('-i', '--func_args', nargs='*', help='args in functioFn', default=[])
+  args = parser.parse_args()
+
+  func_dict = {k: v for k, v in locals().items() if callable(v)}
+  func_args = [float(x) if x.isnumeric() else x for x in args.func_args]
+  ret = func_dict[args.function_name](*func_args)
