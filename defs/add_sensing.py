4e7a5913ed750a4929ce85c96b708fe53545f511
takuma-ru(rasp) <kcattdev@gmail.com>
2022-10-21 02:28:29 +0100

    [Update] Implementation of the process of adding data to the appropriate "ref"

--- defs/add_sensing.py
+++ defs/add_sensing.py
@@ -26,20 +26,37 @@ def add_admin(id: str, temperature: float, humidity: float):
   from firebase_admin import firestore
 
   if (isinstance(id, str) and isinstance(temperature, float) and isinstance(humidity, float)):
+    # 今日の日時を取得し保存
     nowDate = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
-    #nowDate = datetime.datetime(2017, 11, 12, 9, 5, 28)
-    date = str(nowDate.year) + str(nowDate.month) + str(nowDate.day)
+    # nowDate = datetime.datetime(2017, 4, 4, 9, 5, 28)
+    
+    # 今日の年月日を取得し保存(YYYYMMDD)
+    month = '0' + str(nowDate.month)
+    day = '0' + str(nowDate.day)
+    date = str(nowDate.year) + month[-2:] + day[-2:]
     print(date)
+    
+    # 現在の時間（時）を取得し2桁で保存
     hour = '0' + str(nowDate.hour)
-    minute = '0' + str(nowDate.minute)
+    # 現在の時間（分）を取得し、15の倍数でどの値に一番近いかを求め2桁で保存
+    minute = '0' + str(int((round(nowDate.minute / 15, 0) * 15)))
+    if(minute == '060'):
+      # もし minute が '060' だった場合は hour をインクリメントし、minute を '00'にする
+      hour = '0' + str(nowDate.hour + 1)
+      minute = '00'
+
+    # 現在の時分を保存(HHMM)
     time = hour[-2:] + minute[-2:]
     print(time)
+
+    print(f'Add to sensingData/{id}/{date}/{time}')
+
     db = firestore.client()
-    #ref = db.collection(id).document(date).collection(time)
-    #ref.set({
-    #  u'date': datetime.datetime(),
-    #  u'temperature': temperature,
-    #  u'humidity': humidity,
-    #})
+    ref = db.collection('sensingData').document(id).collection(date).document(time)
+    ref.set({
+      u'date': datetime.datetime(str(nowDate.year), str(nowDate.month), str(nowDate.day), 9, 55, 28),
+      u'temperature': temperature,
+      u'humidity': humidity,
+    })
   else:
     print('input variables type not match')
